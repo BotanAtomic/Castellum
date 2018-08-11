94e3cb916d79783a6a05f1bcfe4f00dad2a6b5c8
root <ahmed.botan94@gmail.com>
2018-08-11 20:02:36 +0200

    Review JSONUtils

--- Server/src/org/castellum/json/JSONUtils.java
+++ Server/src/org/castellum/json/JSONUtils.java
@@ -6,17 +6,6 @@ import java.util.Map;
 
 public class JSONUtils {
 
-    private final JSONObject object;
-
-    public JSONUtils(JSONObject object) {
-        this.object = object;
-    }
-
-    public String getString(String data) {
-        return object.getString(data);
-    }
-
-
     public static void implementInMap(Map<String, Object> map, JSONObject object) {
         object.keySet().forEach(key -> {
             Object value = object.get(key);
